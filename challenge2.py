296415f8fa16204c59f972abeeb70b051fc74520
gunika <gunikaj@gmail.com>
2021-06-20 14:58:25 +0530

    Create challenge2.py

---
+++ challenge2.py
@@ -0,0 +1,8 @@
+import boto3
+		
+def ec2InstanceDetails(accessKey, secretAccessKey, instanceId):
+	session = boto3.Session(accessKey, secretAccessKey)
+    service = session.client('ec2')
+    response = service.describe_instances(InstanceIds=[instanceId])
+	return response
+	
