e2c673398caeeeda828669e60c610519ff37d9dd
gunika <gunikaj@gmail.com>
2021-06-20 15:06:09 +0530

    Update challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -1,7 +1,7 @@
 import boto3
 		
 def ec2InstanceDetails(accessKey, secretAccessKey, instanceId):
-	session = boto3.Session(accessKey, secretAccessKey)
+    session = boto3.Session(accessKey, secretAccessKey)
     service = session.client('ec2')
     response = service.describe_instances(InstanceIds=[instanceId])
 	return response
